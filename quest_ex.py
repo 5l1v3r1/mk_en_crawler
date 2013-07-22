9cdda663b131044af1cdc948e750f670235a3309
Mihail <mihail@filip-mkrobot.(none)>
2013-07-22 14:04:56 +0200

    90% english extracted

--- quest_ex.py
+++ quest_ex.py
@@ -13,11 +13,12 @@ def parse_text(xml, i):
     for k in range(0,len(texts)):
         if texts[k]['top'] == '31' or texts[k]['top'] == '1226':
             continue
-        if texts[k]['height'] == '19' and len(texts[k].getText()) > 3:
+        if texts[k]['height'] == '19' and texts[k]['font'] == '4' and len(texts[k].getText().strip()) > 3:
             j += 1
         text = texts[k].getText()
         if len(text) < 3:
-            if len(texts[k-1].findAll('b')) + len(texts[k+1].findAll('b')) > 0:
+            if ((k > 0 and len(texts[k-1].findAll('b')) > 0) or
+               (k < len(texts) - 1 and len(texts[k+1].findAll('b')) > 0)):
                 text = '\n'
             else:
                 text = ''
@@ -26,7 +27,7 @@ def parse_text(xml, i):
         f.close()
 
 def main():
-    parse_text('questionary/1-01 - Political Criteria.xml', 1)
+    parse_text('questionary/3-29 - Financial and budgetary provisions.xml', 31)
 
 if __name__ == '__main__':
     main()
